fbc20df406449b1c728a8e37b121eaca6bc852cd
alexsmaldone <alexsmaldone@gmail.com>
2022-06-07 18:27:16 -0400

    arithemtic formatter setup

--- projects/arithmetic_formatter.py
+++ projects/arithmetic_formatter.py
@@ -1,1 +1,8 @@
 # format the math problems, return the answer if true is passed as an optional argument
+
+
+def arithmetic_arranger(problems, displayedAnswer=False):
+  array_probs = [problem.split(" ") for problem in problems]
+  return array_probs
+
+print(arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"]))
