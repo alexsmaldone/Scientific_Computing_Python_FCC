ec3a1a1b6988cc69986d3258a54b4715cbd9c0b3
alexsmaldone <alexsmaldone@gmail.com>
2022-06-07 23:51:02 -0400

    arithmetic formatter

--- projects/arithmetic_formatter.py
+++ projects/arithmetic_formatter.py
@@ -3,6 +3,18 @@ import operator
 ops = {"+": operator.add, "-": operator.sub}
 
 def arithmetic_arranger(problems, solver=False):
+  top = ""
+  bottom = ""
+  lines = ""
+  totals = ""
+  for n in problems:
+    fnumber = n.split()[0]
+    operator = n.split()[1]
+    snumber = n.split()[2]
 
+    # get function total
+    total = ops[operator](int(fnumber), int(snumber))
+    #  get distance of longeset operator
+    operator_distance = max(len(fnumber), len(snumber)) + 2
 
-print(arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"]))
+arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"])
