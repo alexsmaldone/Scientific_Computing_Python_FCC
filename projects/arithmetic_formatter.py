f227282b9637b1486065bb9a65cebab3fdb73919
alexsmaldone <alexsmaldone@gmail.com>
2022-06-07 23:56:21 -0400

    arithmetic formatter

--- projects/arithmetic_formatter.py
+++ projects/arithmetic_formatter.py
@@ -17,4 +17,17 @@ def arithmetic_arranger(problems, solver=False):
     #  get distance of longeset operator
     operator_distance = max(len(fnumber), len(snumber)) + 2
 
-arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"])
+    # organizing line lengths
+    snumber = operator + snumber.rjust(operator_distance - 1)
+    top = top + fnumber.rjust(operator_distance) + (4 * " ")
+    bottom = bottom + snumber + (4 * " ")
+    lines = lines + len(snumber) * "_" + (4 * " ")
+    totals = totals + str(total).rjust(operator_distance) + (4 * " ")
+
+  print(top)
+  print(bottom)
+  print(lines)
+  if solver:
+    print(totals)
+
+arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"], True)
